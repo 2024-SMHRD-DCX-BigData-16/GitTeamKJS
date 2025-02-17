511cb1c6aa28d7f3761744737f13a6e0f7735f3a
조영재 <dfyd15937@gmail.com>
2025-02-17 16:07:46 +0900

    감자

--- TestTeam/src/ex/Ex01.java
+++ TestTeam/src/ex/Ex01.java
@@ -4,9 +4,12 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("왜 지워 나빴어 영재");
-		System.out.println("핵심프로젝트 잘하자 코딩하면 나는 응원할게");
-		System.out.println("노래해주면 인정해드릴게요");
+		감	자	
+		
+		감		자
+		
+		감			자
+		
 	}
 
 }
