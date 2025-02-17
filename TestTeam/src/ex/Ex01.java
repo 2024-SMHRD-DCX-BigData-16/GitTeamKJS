ef92c9d838b4feadca5a1981a44bb1ac3cc00c41
uni <moonrit001@gmail.com>
2025-02-17 15:56:52 +0900

    6개짼가?

--- TestTeam/src/ex/Ex01.java
+++ TestTeam/src/ex/Ex01.java
@@ -4,11 +4,12 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("팀장님은 왜 적은게 없죠");
+		System.out.println("팀장 첫번째 커밋");
 		System.out.println("주석형 하이요");
+		System.out.println("팀장님은 왜 적은게 없죠");
 		System.out.println("미용실 갈건데 스타일 추천좀 부탁해욥");
 		System.out.println("메롱");
-		System.out.println("팀장 첫번째 커밋");
+		System.out.println("메롱머린 없는데요,,,?");
 		
 	}
 
