6c436569cad13a3a62c52975f2a231ab5b92a9ca
KJS <2690149@naver.com>
2025-02-17 15:55:09 +0900

    test

--- TestTeam/src/ex/Ex01.java
+++ TestTeam/src/ex/Ex01.java
@@ -7,7 +7,7 @@ public class Ex01 {
 		System.out.println("팀장님은 왜 적은게 없죠");
 		System.out.println("주석형 하이요");
 		System.out.println("미용실 갈건데 스타일 추천좀 부탁해욥");
-
+		System.out.println("메롱");
 		System.out.println("팀장 첫번째 커밋");
 		
 	}
