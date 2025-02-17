17a4a017f15a76bdb58dc0edc26dea7eb4947374
KJS <2690149@naver.com>
2025-02-17 16:21:27 +0900

    주석

--- TestTeam/src/ex/Ex01.java
+++ TestTeam/src/ex/Ex01.java
@@ -4,8 +4,7 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("메롱");
-		System.out.println("핥짝");
+		System.out.println("다들 자격증 시험 화이팅하고 ADSP 5일 용사 갑시다");
 		
 	}
 
