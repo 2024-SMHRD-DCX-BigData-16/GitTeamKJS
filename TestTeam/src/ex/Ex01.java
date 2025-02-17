936ab6eccd7a3fbaf95b5437007499af296e21f1
uni <moonrit001@gmail.com>
2025-02-17 15:49:11 +0900

    팀원2커밋

--- TestTeam/src/ex/Ex01.java
+++ TestTeam/src/ex/Ex01.java
@@ -4,7 +4,9 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("팀장님은 왜 적은게 없죠");
 		System.out.println("주석형 하이요");
+		System.out.println("미용실 갈건데 스타일 추천좀 부탁해욥");
 
 	}
 
